fc03ff66aefae1c7db71d66ee38acae74e2f755a
borovirp <40pobGsb>
2022-02-21 11:24:23 +0300

    new update

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 # Распарсить из статьи https://en.wikipedia.org/wiki/Bias-variance_tradeoff все заголовки верхнего уровня
 import requests
 import bs4
+
 print('Новости по djano:')
-req = requests.get('http://pythondigest.ru/feed/?q=django', verify = False)
+req = requests.get('http://pythondigest.ru/feed/?q=django', verify=False)
 print(req)
 parser = bs4.BeautifulSoup(req.text, 'lxml')
-#Выделим все заголовки четвертого уровня - тег h4:
+# Выделим все заголовки четвертого уровня - тег h4:
 y = parser.findAll('a', href=True, text=True)
-with open('output.txt','wt') as f:
+with open('output.txt', 'wt') as f:
     for result in y:
-        f.write('h1 tag'+'link_text'+result['href'])
+        print('h1 tag', 'link_text', result['href'])
+        f.write('h1 tag' + 'link_text' + result['href'])
